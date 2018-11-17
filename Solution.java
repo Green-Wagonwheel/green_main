5145a83a2448153c5f4a4eaecd4a370c553b6f0e
Green-Wagonwheel <green.wagonwheel@gmail.com>
2018-11-16 19:26:45 -0800

    first commit

---
+++ Solution.java
@@ -0,0 +1,13 @@
+
+public class Solution {
+
+	public static void main(String[] args) {
+		System.out.println("Hello team!");
+		System.out.println("Gosling is here!");
+		System.out.println("Red-Wagonwheel is here!");
+		System.out.println("Green-Wagonwheel is here!");
+		System.out.println("Can not push green branch to origin");
+
+	}
+
+}
